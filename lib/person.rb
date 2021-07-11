6efea98b04104878f05c2652df7635f1a36a8a86
chris <cseiter@gmail.com>
2021-07-11 14:32:58 -0400

    call_friend method passes initial tests

--- lib/person.rb
+++ lib/person.rb
@@ -44,7 +44,11 @@ class Person
         "♪ another one bites the dust ♫"
     end
 
-
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi friend.name! It's self.name. How are you?"
+    end
 
 
 
