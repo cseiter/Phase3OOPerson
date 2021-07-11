07ff25d193cfa4701d4631858b831ca8514c3538
chris <cseiter@gmail.com>
2021-07-11 14:53:31 -0400

    start_conversation method created.  All tests pass

--- lib/person.rb
+++ lib/person.rb
@@ -51,11 +51,17 @@ class Person
     end
 
     def start_conversation(friend,topic)
-        
-
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
     end
-
-
-
-
 end
