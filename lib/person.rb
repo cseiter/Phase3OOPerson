fc5f91c43de89dd20bef0e65ababb6c42eb5d15e
chris <cseiter@gmail.com>
2021-07-11 14:11:12 -0400

    set the attr items to correct naming convention to pass tests

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,14 @@
 #clamp https://stackoverflow.com/questions/20352737/how-to-restrict-an-integer-to-a-range-in-ruby
 class Person
 
-    attr_accessor :balance,:happy,:hygiene
+    attr_accessor :bank_account,:happiness,:hygiene
     attr_reader :name
 
     def initialize(name)
-        @name=name      #read-only
-        @balance=25     #any
-        @happy=8        #clamp between 0 and 10
-        @hygiene=8      #clamp between 0 and 10
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
 
 
