7fbeb8352511cff4edce8982e4f4cb55e4e74f34
chris <cseiter@gmail.com>
2021-07-11 14:07:38 -0400

    rename attributes to match tests and set name as read-only

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,12 @@
 #clamp https://stackoverflow.com/questions/20352737/how-to-restrict-an-integer-to-a-range-in-ruby
 class Person
 
-    attr_accessor :name,:balance,:happy,:hygiene
+    attr_accessor :balance,:happy,:hygiene
+    attr_reader :name
 
     def initialize(name)
-        @name=name
-        @balance=2500   #any, in cents
+        @name=name      #read-only
+        @balance=25     #any
         @happy=8        #clamp between 0 and 10
         @hygiene=8      #clamp between 0 and 10
     end
