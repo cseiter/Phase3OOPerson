fb201b14bcf1a33f8ed311618050f85441da44ab
chris <cseiter@gmail.com>
2021-07-11 14:01:02 -0400

    create Person class with beginning initializations

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # your code goes here
+#clamp https://stackoverflow.com/questions/20352737/how-to-restrict-an-integer-to-a-range-in-ruby
+class Person
+
+    attr_accessor :name,:balance,:happy,:hygiene
+
+    def initialize(name)
+        @name=name
+        @balance=2500   #any, in cents
+        @happy=8        #clamp between 0 and 10
+        @hygiene=8      #clamp between 0 and 10
+    end
+
+
+
+
+
+
+
+
+
+
+end
